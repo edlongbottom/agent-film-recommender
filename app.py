418294c2a645aede8433f9389878fe18f3670745
ed-longbottom <ed.m.longbottom@bbc.co.uk>
2025-06-20 10:28:12 +0100

    basic scaffold film recommender

---
+++ app.py
@@ -0,0 +1,39 @@
+import yaml
+
+from gradio_ui import GradioUI
+
+from smolagents import CodeAgent, HfApiModel
+from tools.film_recommender import FilmRecommender
+
+# load the model from Hugging Face
+model = HfApiModel(
+    max_tokens=2096,
+    temperature=0.5,
+    model_id="Qwen/Qwen2.5-Coder-32B-Instruct",  # it is possible that this model may be overloaded
+    custom_role_conversions=None,
+)
+
+# define prompt templates
+with open("prompts.yaml", "r") as stream:
+    prompt_templates = yaml.safe_load(stream)
+
+# define tools
+film_recommender = FilmRecommender()
+
+tools = [film_recommender]
+
+# define code agent
+agent = CodeAgent(
+    model=model,
+    tools=tools,
+    max_steps=6,
+    verbosity_level=1,
+    grammar=None,
+    planning_interval=None,
+    name=None,
+    description=None,
+    prompt_templates=prompt_templates,
+)
+
+# launch the app
+GradioUI(agent).launch()
---
+++ tools/film_recommender.py
@@ -0,0 +1,57 @@
+import json
+import random
+
+from smolagents import Tool
+
+
+class FilmRecommender(Tool):
+    name = "film_recommender"
+    description = """
+    Suggests a film based on user preferences such as genre, mood or score.
+    Returns a film title, a short description, and a mock IMDB score out of 10.
+    """
+
+    inputs = {
+        "genre": {
+            "type": "string",
+            "description": "The genre of the film (e.g., Action, Comedy, Drama).",
+            "required": False,
+        },
+        "mood": {
+            "type": "string",
+            "description": "User's current mood or desired film mood (e.g., 'uplifting', 'thrilling').",
+            "required": False,
+        },
+        "score": {
+            "type": "float",
+            "description": "Film IMDB score threshold (between 0 to 10) above which the user expects the film to score.",
+            "required": False,
+        },
+    }
+
+    output_type = "string"
+
+    def forward(
+        self,
+        genre: str | None = None,
+        mood: str | None = None,
+        score: float | None = None,
+    ) -> str:
+        # load in films from the JSON file
+        with open("data/example_films.json", "r") as file:
+            films = json.load(file)
+
+        # implement filter logic
+        filtered = films
+        if genre:
+            filtered = [m for m in filtered if genre.lower() in m["genre"]]
+        if mood and filtered:
+            filtered = [m for m in filtered if mood.lower() in m["mood"]]
+        if score and filtered:
+            filtered = [m for m in filtered if m["score"] >= score]
+
+        if not filtered:
+            filtered = films  # fallback to all
+
+        film = random.choice(filtered)
+        return f"You might enjoy '{film['title']}': {film['desc']} IMDB score: {film['score']}/10."
